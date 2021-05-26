b4d412a11a3cad963b179b42a5edb389df63d736
Vidhi Jain <60235738+vidhsss@users.noreply.github.com>
2021-05-26 06:57:29 +0530

    Create movies.py

---
+++ movies.py
@@ -0,0 +1,9 @@
+import pandas 
+df= pd.read_csv(url,sep=",")
+def get_title_from_index(index):
+      return df.loc[index, "original_title"]
+def get_rating_from_index(index):
+  return df.loc[index, "original_title"],df.loc[index, "reviews_from_users"]
+
+def get_index_from_title(original_title):
+  return df.loc[df.original_title == original_title].index[0]
